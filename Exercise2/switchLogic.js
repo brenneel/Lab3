1198b472d2bae0180d256858bc68d877f99e8092
Ethan <ethanlbrenner@gmail.com>
2019-09-27 15:08:58 -0500

    implemented onPageLoad()

--- Exercise2/switchLogic.js
+++ Exercise2/switchLogic.js
@@ -1,9 +1,20 @@
+let arr;
 //advances the slide show forward
 function next(){
-
+    document.getElementById("image").src = arr[1];
 }
 
 //goes back a slide
 function previous(){
 
+}
+
+function onPageLoad(){
+    arr = [
+    "./images/red.png",
+    "./images/lightBlue.png",
+    "./images/oceanBlue.png",
+    "./images/orange.png",
+    "./images/lightGreen"
+    ];
 }

50998860fc019d72c4b6f6f1359924ae208adf04
Ethan <ethanlbrenner@gmail.com>
2019-09-27 14:40:32 -0500

    fixed verifyInput() logic

--- Exercise1/validator.js
+++ Exercise1/validator.js
@@ -1,16 +1,16 @@
 //verifies the input from the two text boxes 1) is valid and 2) matches each other
 function verifyInput(){
-    let input1 = document.getElementById("passInput");
-    let input2 = document.getElementById("verifyInput");
+    let input1 = document.getElementById("passInput").value;
+    let input2 = document.getElementById("verifyInput").value;
     
     if(input1.length <8){
         alert("Your password is too short!");
     }
-    if(input1 == input2){
-        //do nothing
+    else if(input1 !== input2){
+        alert("Your passwords do not match, type carefully!");
     }
     else{
-        alert("Your passwords do not match, type carefully!");
+        //do nothing, the passwords are valid
     }
     
 }

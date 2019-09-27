0b9e6b9cdb5d083d5722585c876f20884586efc9
Ethan <ethanlbrenner@gmail.com>
2019-09-27 17:10:50 -0500

    fixed switcher function

--- Exercise2/switchLogic.js
+++ Exercise2/switchLogic.js
@@ -1,10 +1,17 @@
 let arr;
 let count = 0;
+
 //advances the slide show forward
 function next(){
+    count = count +1;
+   
     if(count == 4){
         document.getElementById("image").src = arr[4];
         count = -1;
+    }
+    else if(count >4){
+        document.getElementById("image").src = arr[0];
+        count = 0;
     }
     else{
         document.getElementById("image").src = arr[count];
@@ -15,15 +22,17 @@ function next(){
 
 //goes back a slide
 function previous(){
+    count = count - 1;
     if(count == 0){
         document.getElementById("image").src = arr[0];
         count = 5;
     }
-    else
-    {
+    else if(count <0){
+        document.getElementById("image").src = arr[4];
+        count = 4;
+    }
+    else{
         document.getElementById("image").src = arr[count];
-        
-        
     }
 }
 //handles direction switching
@@ -31,15 +40,17 @@ function previous(){
 //false means reverse
 function switcher(forward){
     if(forward == true){
+        
         next();
-        count = count + 1;
+        
     }
     else{
-        count = count - 1;
+        
         previous();
     }
 }
 
+//loads images into the array when the page loads in
 function onPageLoad(){
     arr = [
     "./images/red.png",

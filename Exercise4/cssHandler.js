39644a0e504b3508cbc3759e3a1582e2d4faf81f
Ethan <ethanlbrenner@gmail.com>
2019-10-01 13:36:12 -0500

    fixed spelling issues and adding submitColors

--- Exercise4/cssHandler.js
+++ Exercise4/cssHandler.js
@@ -1,6 +1,11 @@
 //changes the css elements using js
 function submitColors(){
     var myParagraph = document.getElementById("dummyText");
+    var borderRed = document.getElementByI("redBorder").value;
+    var borderGreen = document.getElementById("greenBorder").value;
+    var borderBlue = document.getElementById("blueBorder").value;
+    var borderWidth = document.getElementById("width").value;
 
-    
+    myParagraph.style.backgroundColor =rgb(borderRed,borderGreen,borderBlue);
+    myParagraph.style.borderWidth = borderWidth; 
 }
